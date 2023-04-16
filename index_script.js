f7b56ea5d9d0e83d681af721105904a9c0d654b5
Saransh Kumar <saransh.kumar0707@gmail.com>
2023-04-16 16:39:32 +0530

    Updating for user interface

--- index_script.js
+++ index_script.js
@@ -1,56 +1,104 @@
-const cases = ["ROCK", "PAPER", "SCISSORS"];
+let cScore = 0;
+let pScore = 0;
+
+const resultContainer = document.querySelector('#resultContainer');
+
+const scoreMeter = document.createElement('div');
+scoreMeter.className = 'scoreMeter';
+resultContainer.appendChild(scoreMeter);
+
+const roundResult = document.createElement('div');
+roundResult.className = 'roundResult';
+resultContainer.appendChild(roundResult);
+
+const finalResult = document.createElement('div');
+finalResult.className = 'finalResult';
+resultContainer.appendChild(finalResult);
 
 function getComputerChoice() {
     var x = Math.floor(Math.random() * 10);
     x %= 3;
 
+    const cases = ["ROCK", "PAPER", "SCISSORS"];
     return cases[x];
 }
 
-function playRound(pSelect, cSelect) {
-    pSelect = pSelect.toUpperCase();
-    cSelect = cSelect.toUpperCase();
+function overCondition(playerWon) {
+    if (playerWon)
+        pScore++;
+    else
+        cScore++;
+
+    scoreMeter.textContent = `Player Score: ${pScore}
+                                Computer Score: ${cScore}`;
+
+    console.log('pScore: ' + pScore);
+    console.log('cScore: ' + cScore);
+
+    if (pScore >= 5) {
+        finalResult.textContent = "Player Won the Game";
+    }
+    else if (cScore >= 5) {
+        finalResult.textContent = "Player Lost the Game";
+    }
+    else
+        return;
     
+    pScore = 0;
+    cScore = 0;
+}
+
+function playRound(event) {
+    let pSelect = this.className.toUpperCase();
+    let cSelect = getComputerChoice();
+
+    console.log("pSelect: " + pSelect);
+    console.log("cSelect: " + cSelect);
+
     var res = "You ";
 
-    if(cSelect === 'ROCK')
-    {
-        if(pSelect === 'ROCK')
+    if (cSelect === 'ROCK') {
+        if (pSelect === 'ROCK')
             res = res + "Tied";
-        else if(pSelect === 'PAPER')
+        else if (pSelect === 'PAPER') {
             res = res + "Win, Paper beats Rock";
-        else if(pSelect === 'SCISSORS')
+            overCondition(true);
+        }
+        else if (pSelect === 'SCISSORS') {
             res = res + "Lose, Rock beats Scissors";
+            overCondition(false);
+        }
     }
-    else if(cSelect === 'PAPER')
-    {
-        if(pSelect === 'ROCK')
+    else if (cSelect === 'PAPER') {
+        if (pSelect === 'ROCK') {
             res = res + "Lose, Paper beats Rock";
-        else if(pSelect === 'PAPER')
+            overCondition(false);
+        }
+        else if (pSelect === 'PAPER')
             res = res + "Tied";
-        else if(pSelect === 'SCISSORS')
+        else if (pSelect === 'SCISSORS') {
             res = res + "Win, Scissors beats Paper";
+            overCondition(true);
+        }
     }
-    else if(cSelect === 'SCISSORS')
-    {
-        if(pSelect === 'ROCK')
+    else if (cSelect === 'SCISSORS') {
+        if (pSelect === 'ROCK') {
             res = res + "Win, Rock beats Scissors";
-        else if(pSelect === 'PAPER')
+            overCondition(true);
+        }
+        else if (pSelect === 'PAPER') {
             res = res + "Lose, Scissors beats Paper";
-        else if(pSelect === 'SCISSORS')
+            overCondition(false);
+        }
+        else if (pSelect === 'SCISSORS')
             res = res + "Tied";
     }
 
-    return res;
+    roundResult.textContent = res;
 }
 
-function game(count) {
-    for (var i = 0; i < count; i++) {
-        const pSelect = prompt();
-        const cSelect = getComputerChoice();
+const buttons = document.querySelectorAll('button');
 
-        console.log(playRound(pSelect, cSelect));
-    }
-}
-
-game(5);
+buttons.forEach((btn) => {
+    btn.addEventListener('click', playRound);
+});
